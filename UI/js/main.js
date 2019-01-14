6c0a497665eddf73c64f93af68965ed09611dadd
mnswaleh <mnswaleh@gmail.com>
2019-01-14 20:54:37 +0300

    include user logout

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,4 +1,4 @@
-const SERVER = "https://api-sendit.herokuapp.com/api/v2/";
+const SERVER = "http://127.0.0.1:5000/api/v2/";
 
 let params = new URLSearchParams(location.search);
 let access_token = localStorage.getItem('token');
@@ -22,4 +22,34 @@ function FormDataToJSON(FormElement) {
     }
 
     return JSON.stringify(ConvertedJSON);
+}
+
+function logout(){
+    let myPost = {
+        method: 'PUT',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    let request = new Request(SERVER + 'auth/logout', myPost);
+
+    fetch(request).then(function (response) {
+        if (response.status !== 200 && response.status !== 403) {
+            window.location.replace('sign-in.html');
+        }else{
+            return response.json();
+        }
+    }).then(function (myresponse) {
+        if (myresponse.message) {
+            localStorage.setItem('token', myresponse.access);
+            localStorage.setItem('user', myresponse.user);
+            location.reload(true);
+        } else if (myresponse.ERROR) {
+            alert(myresponse.ERROR);
+        } 
+        else {
+            alert('Ooops! Something went wrong');
+        }
+    });
 }
