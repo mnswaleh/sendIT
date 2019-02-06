bba203139ceeff406d019ba0d7f2c3537def1145
mnswaleh <mnswaleh@gmail.com>
2019-02-07 00:43:45 +0300

    [finishes #162441243] fix user permission

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,9 +1,11 @@
-const SERVER = "https://api-sendit.herokuapp.com/api/v2/";
+const SERVER = "http://127.0.0.1:5000/api/v2/";
 
 let myresponse = {};
 let params = new URLSearchParams(location.search);
 let access_token = localStorage.getItem('token') || "FDFAXAF&/SA/(AB>VAB";
 let user_info = localStorage.getItem('user') || [0, "user"];
+
+user_info = user_info.split(',');
 
 let myHeaders = new Headers();
 
